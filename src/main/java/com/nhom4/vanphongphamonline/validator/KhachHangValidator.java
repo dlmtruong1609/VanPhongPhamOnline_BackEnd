9436450ce9ead1301635a73cdbe606e4c3d5c918
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2020-03-12 20:16:44 +0700

    fix khachHang validator

--- src/main/java/com/nhom4/vanphongphamonline/validator/KhachHangValidator.java
+++ src/main/java/com/nhom4/vanphongphamonline/validator/KhachHangValidator.java
@@ -24,18 +24,18 @@ public class KhachHangValidator implements Validator{
 		// TODO Auto-generated method stub
 		KhachHang khachHang = (KhachHang) target;
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "tenKhachHang", "Tên khách hàng không bỏ trống", "1");
-		if(khachHang.getTenKhachHang().length() < 3 || khachHang.getTenKhachHang().length() > 50) {
+		if(khachHang.getTenKhachHang() != null && khachHang.getTenKhachHang().length() < 3 || khachHang.getTenKhachHang().length() > 50) {
 			errors.rejectValue("tenKhachHang", "Tên khách hàng không được dưới 3 kí tự và không lớn hơn 50 kí tự", "3");
 		}
 		
-		if(khachHang.getDienThoai() != null) {
+		if(khachHang.getDienThoai() != null && khachHang.getDienThoai() != null) {
 			String regex = "^0[0-9]{9}$"; // 10 số và bắt đầu là 0
 			if(khachHang.getDienThoai().matches(regex)!=true) {
 				errors.rejectValue("dienThoai", "Sai định dạng số điện thoại", "4");
 			}
 		}
 		String regex = "^[0-9]{9}$|^[0-9]{11}$"; // cmnd có 9 hoặc 11 kí tự (9 cmnd, 11 là căn cước)
-		if(khachHang.getCmnd().matches(regex)!=true) {
+		if(khachHang.getCmnd() != null && khachHang.getCmnd().matches(regex)!=true) {
 			errors.rejectValue("cmnd", "Sai định dạng CMND", "5");
 		}
 	}
