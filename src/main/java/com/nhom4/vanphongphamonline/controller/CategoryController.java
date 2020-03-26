fc4e015e5aa34bf2c0e187b73129a193694f9857
Đặng Lê Minh Trường <dlmtruong1609@gmail.com>
2020-03-26 13:18:57 +0700

    add api CRUD supplier and category

--- src/main/java/com/nhom4/vanphongphamonline/controller/CategoryController.java
+++ src/main/java/com/nhom4/vanphongphamonline/controller/CategoryController.java
@@ -3,13 +3,20 @@ package com.nhom4.vanphongphamonline.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nhom4.vanphongphamonline.model.Category;
+import com.nhom4.vanphongphamonline.model.Product;
+import com.nhom4.vanphongphamonline.model.Supplier;
 import com.nhom4.vanphongphamonline.repository.CategoryRepository;
 import com.nhom4.vanphongphamonline.repository.OrderRepository;
 import com.nhom4.vanphongphamonline.services.ServiceStatus;
@@ -23,15 +30,56 @@ public class CategoryController {
 	public CategoryController(CategoryRepository categoryRepository) {
 		this.categoryRepository = categoryRepository;
 	}
+	@ResponseBody
+	@PostMapping(value = "/api/v1/admin/category/add")
+	public ResponseEntity<ServiceStatus> addCategory(@RequestBody Category category) { // chưa bắt valid
+		categoryRepository.insert(category);
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Thêm loại sản phẩm thành công"), HttpStatus.OK);
+		
+	}
+
+	@ResponseBody
+	@PostMapping(value = "/api/v1/admin/category/delete")
+	public ResponseEntity<ServiceStatus> deleteCategoryById(@RequestParam String id) {
+		if(categoryRepository.findById(id).isPresent()!=false) {
+			categoryRepository.deleteById(id);
+		} else {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Loại sản phẩm không tồn tại"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Xoá loại sản phẩm thành công", categoryRepository.findAll()), HttpStatus.OK);
+	}
+	@ResponseBody
+	@PostMapping(value = "/api/v1/admin/category/update")
+	public ResponseEntity<ServiceStatus> updateCategoryById(@RequestParam String id, @RequestBody Category category) { // chưa kiểm tra valid
+		if(categoryRepository.findById(id).isPresent()!=false) {
+			Category categoryUpdated = categoryRepository.findById(id).get();
+			categoryUpdated.setName(category.getName());
+			categoryRepository.save(categoryUpdated);
+		} else {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Loại sản phẩm không tồn tại"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Cập nhật loại sản phẩm thành công sản phẩm thành công"), HttpStatus.OK);
+	}
 	
 	@ResponseBody
-	@GetMapping(value = "/api/v1/category/list")
-	public ResponseEntity<ServiceStatus> getAllCategory() {
-		List<Category> list = categoryRepository.findAll();
-		if(list == null) {
-			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Không có category"), HttpStatus.OK);
+	@GetMapping(value = "/api/v1/category/page") // phân trang
+	public ResponseEntity<ServiceStatus> getCategoryPageByIndex(@RequestParam int index) {
+		Page<Category> page = categoryRepository.findAll(PageRequest.of(index, 12)); // 1 page có 12 sản phẩm
+		if(page == null) {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Không có loại sản phẩm nào"), HttpStatus.OK);
 		}
-		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Danh sách category", list), HttpStatus.OK);
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Trang " + index, page), HttpStatus.OK);
+	}
+	
+	//	Tim kiem text search, tạo index trước
+	@ResponseBody //// db.category.ensureIndex({ name: "text"});
+	@GetMapping(value = "/api/v1/category/search") // seacrh có phân trang
+	public ResponseEntity<ServiceStatus> search(@RequestParam int index, @RequestParam String keyword) {
+		Page<Category> page = categoryRepository.findByTextSearch(keyword, PageRequest.of(index, 12)); // 1 page có 12 sản phẩm
+		if(page == null) {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Không có loại sản phẩm"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Trang " + index, page), HttpStatus.OK);
 	}
 
 }
--- src/main/java/com/nhom4/vanphongphamonline/controller/SupplierController.java
+++ src/main/java/com/nhom4/vanphongphamonline/controller/SupplierController.java
@@ -3,14 +3,26 @@ package com.nhom4.vanphongphamonline.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.nhom4.vanphongphamonline.model.Category;
+import com.nhom4.vanphongphamonline.model.Product;
 import com.nhom4.vanphongphamonline.model.Supplier;
 import com.nhom4.vanphongphamonline.repository.SupplierRepository;
+import com.nhom4.vanphongphamonline.services.ServiceStatus;
 
 @Controller
 public class SupplierController {
@@ -21,15 +33,65 @@ public class SupplierController {
 		// TODO Auto-generated constructor stub
 	}
 	@ResponseBody
-	@RequestMapping(value = "/nhacungcap/add", method = RequestMethod.POST)
-	public String addNhaCungCap(@RequestParam("description")String description, @RequestParam("id") String id, @RequestParam("name") String name) {
-		Supplier supplier = new Supplier(id, name, description);
+	@PostMapping(value = "/api/v1/admin/supplier/add")
+	public ResponseEntity<ServiceStatus> addSupplier(@RequestBody Supplier supplier) { // chưa bắt valid
 		supplierRepository.insert(supplier);
-		return "Đã thêm" + supplier;
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Thêm nhà cung cấp thành công"), HttpStatus.OK);
+		
 	}
 	@ResponseBody
-	@RequestMapping("/nhacungcap/data")
-	public List<Supplier> getAllNhaCungCap() {
-		return supplierRepository.findAll();
+	@GetMapping(value = "/api/v1/supplier/list")
+	public ResponseEntity<ServiceStatus> getAllSupplier() {
+		List<Supplier> list = null;
+		list = supplierRepository.findAll();
+		if(list == null) {
+			return new ResponseEntity<ServiceStatus>( new ServiceStatus(1, "Không có nhà cung cấp tồn tại"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>( new ServiceStatus(0, "Danh sách nhà cung cấp", list), HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@PostMapping(value = "/api/v1/admin/supplier/delete")
+	public ResponseEntity<ServiceStatus> deleteSupplierById(@RequestParam String id) {
+		if(supplierRepository.findById(id).isPresent()!=false) {
+			supplierRepository.deleteById(id);
+		} else {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Nhà cung cấp không tồn tại"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Xoá nhà cung cấp thành công", supplierRepository.findAll()), HttpStatus.OK);
+	}
+	@ResponseBody
+	@PostMapping(value = "/api/v1/admin/supplier/update")
+	public ResponseEntity<ServiceStatus> updateProductById(@RequestParam String id, @RequestBody Supplier supplier) { // chưa kiểm tra valid
+		if(supplierRepository.findById(id).isPresent()!=false) {
+			Supplier supplierUpdated = supplierRepository.findById(id).get();
+			supplierUpdated.setName(supplier.getName());
+			supplierUpdated.setDescription(supplier.getDescription());
+
+			supplierRepository.save(supplierUpdated);
+		} else {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Nhà cung cấp không tồn tại"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Cập nhật nhà cung cấp thành công sản phẩm thành công"), HttpStatus.OK);
+	}
+	
+	@ResponseBody
+	@GetMapping(value = "/api/v1/supplier/page") // phân trang
+	public ResponseEntity<ServiceStatus> getSupplierPageByIndex(@RequestParam int index) {
+		Page<Supplier> page = supplierRepository.findAll(PageRequest.of(index, 12)); // 1 page có 12 sản phẩm
+		if(page == null) {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Không có nhà cung cấp nào"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Trang " + index, page), HttpStatus.OK);
+	}
+	//	Tim kiem text search, tạo index trước
+	@ResponseBody //// db.supplier.ensureIndex({ name: "text", description: "text"});
+	@GetMapping(value = "/api/v1/supplier/search") // seacrh có phân trang
+	public ResponseEntity<ServiceStatus> search(@RequestParam int index, @RequestParam String keyword) {
+		Page<Supplier> page = supplierRepository.findByTextSearch(keyword, PageRequest.of(index, 12)); // 1 page có 12 sản phẩm
+		if(page == null) {
+			return new ResponseEntity<ServiceStatus>(new ServiceStatus(1, "Không có nhà cung cấp"), HttpStatus.OK);
+		}
+		return new ResponseEntity<ServiceStatus>(new ServiceStatus(0, "Trang " + index, page), HttpStatus.OK);
 	}
 }
